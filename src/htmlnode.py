db693b56b37daa7330328098e42665d3da7147ef
Bakr101 <ahmed98bakr@gmail.com>
2025-01-23 11:41:27 +0200

    parent node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,6 +23,31 @@ class HTMLNode:
         return self.tag == other.tag and self.value == other.value and self.children == other.children and self.props == other.props
     
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict[str, str] = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str, children: list['HTMLNode'], props: dict[str, str] = None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        return f"<{self.tag}{self.props_to_html()}>{''.join([child.to_html() for child in self.children])}</{self.tag}>"
     
     
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
@@ -24,5 +25,58 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("div", "This is a div", [], {"class": "container", "id": "main", "data-id": "123"})
         self.assertEqual(node.props_to_html(), " class=\"container\" id=\"main\" data-id=\"123\"")
 
+    def test_leaf_node(self):
+        node = LeafNode("div", "This is a div", {"class": "container", "id": "main", "data-id": "123"})
+        self.assertEqual(node.to_html(), "<div class=\"container\" id=\"main\" data-id=\"123\">This is a div</div>")
+    
+    def test_leaf_node_no_tag(self):
+        node = LeafNode(None, "This is a div", {"class": "container", "id": "main", "data-id": "123"})
+        self.assertEqual(node.to_html(), "This is a div")
+    
+    def test_leaf_node_no_props(self):
+        node = LeafNode("div", "This is a div", {})
+        self.assertEqual(node.to_html(), "<div>This is a div</div>")
+    
+    def test_leaf_node_repr(self):
+        node = LeafNode("div", "This is a div", {"class": "container", "id": "main", "data-id": "123"})
+        self.assertEqual(repr(node), "LeafNode(div, This is a div, {'class': 'container', 'id': 'main', 'data-id': '123'})")
+    
+    def test_leaf_node_no_value(self):
+        node = LeafNode("div", None, {"class": "container", "id": "main", "data-id": "123"})
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
+    def test_parent_node(self):
+        node = ParentNode("div", [LeafNode("span", "This is a span", {"class": "container", "id": "main", "data-id": "123"}), LeafNode("span", "This is a span", {"class": "container", "id": "main", "data-id": "123"})], {"class": "container", "id": "main", "data-id": "123"})
+        self.assertEqual(node.to_html(), "<div class=\"container\" id=\"main\" data-id=\"123\"><span class=\"container\" id=\"main\" data-id=\"123\">This is a span</span><span class=\"container\" id=\"main\" data-id=\"123\">This is a span</span></div>")
+    
+    def test_parent_node_no_tag(self):
+        parent = ParentNode(None, [LeafNode("span", "This is a span", {"class": "container", "id": "main", "data-id": "123"}), LeafNode("span", "This is a span", {"class": "container", "id": "main", "data-id": "123"})], {"class": "container", "id": "main", "data-id": "123"})
+        with self.assertRaises(ValueError):
+            parent.to_html()
+    
+    def test_parent_node_no_children(self):
+        parent = ParentNode("div", None, {"class": "container", "id": "main", "data-id": "123"})
+        with self.assertRaises(ValueError):
+            parent.to_html()
+    
+    def test_parent_node_multiple_children(self):
+        node = ParentNode("div", [LeafNode("span", "This is a span", {"class": "container", "id": "main", "data-id": "123"}), LeafNode("span", "This is a span", {"class": "container", "id": "main", "data-id": "123"}), LeafNode("span", "This is a span", {"class": "container", "id": "main", "data-id": "123"}), LeafNode("span", "This is a span", {"class": "container", "id": "main", "data-id": "123"}), LeafNode("span", "This is a span", {"class": "container", "id": "main", "data-id": "123"})], {"class": "container", "id": "main", "data-id": "123"})
+        self.assertEqual(node.to_html(), "<div class=\"container\" id=\"main\" data-id=\"123\"><span class=\"container\" id=\"main\" data-id=\"123\">This is a span</span><span class=\"container\" id=\"main\" data-id=\"123\">This is a span</span><span class=\"container\" id=\"main\" data-id=\"123\">This is a span</span><span class=\"container\" id=\"main\" data-id=\"123\">This is a span</span><span class=\"container\" id=\"main\" data-id=\"123\">This is a span</span></div>")
+
+    def test_headings(self):
+        node = ParentNode(
+            "h2",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
+        )
 if __name__ == "__main__":
     unittest.main()
